795814a6ac19ec94beeff66aa05aa6f2bfa67bb4
mayfire71 <stevenma@limabean.co.za>
2017-03-09 14:32:00 +0200

    Update tournament.sql

--- tournament/tournament.sql
+++ tournament/tournament.sql
@@ -5,7 +5,7 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
-DROP view player_info;
+DROP view player_wins;
 
 Drop table Matches cascade;
 
@@ -19,6 +19,15 @@ CREATE table Players (ID serial primary key, fullname text) ;
 CREATE table Matches (match_ID serial primary key, winner integer references Players(ID), loser integer references Players(ID) ); 
 
 
-CREATE VIEW player_wins AS
-		SELECT Players.ID, Players.fullname, count(Matches.winner) as wins,
-		count(Matches.match_ID) as Matches from Players full join Matches on Players.ID = Matches.winner group by Players.ID;
+ CREATE VIEW player_wins AS
+ 		SELECT players.id, players.fullname,
+       (SELECT Count(matches.winner)
+        FROM   matches
+        WHERE  matches.winner = players.id) AS wins,
+       (SELECT Count(matches.match_ID)
+        FROM   matches
+        WHERE  matches.loser = players.id
+                OR matches.winner = players.id) AS matches
+FROM  players
+
+
